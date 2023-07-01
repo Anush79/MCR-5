a7b052a6a588bbc6f8b905bcd3552aede1da9a01
Anush79 <barbieworld1056.a@gmail.com>
2023-07-01 18:42:22 +0530

    added mui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 
 function App() {
   return (
@@ -8,10 +9,12 @@ function App() {
         <h1 className="text-5xl font-bold underline text-red-400">
       Hello world!
     </h1>
+    
     <p>
       lorem30
 
     </p>
+    <ChatBubbleIcon/>
       </main>
          
     </div>
